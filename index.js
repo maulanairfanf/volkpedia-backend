373d0713338b314e5066180137e228a6057802de
maulanairfanf <maulanairfanf@gmail.com>
2023-12-20 20:15:19 +0700

    CRUD Product

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
 const app = express()
-const port = 3000
+const productRouter = require('./routes/products')
+const port = 3000;
 
-app.get('/', (req, res) => res.send("Hello World!"))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+dotenv.config()
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("db connected"))
+.catch((err) => console.log(err))
+
+app.use(express.json({limit: '10mb'}));
+app.use(express.urlencoded({limit: '10mb', extended: true}))
+
+app.use('/api/products', productRouter)
+app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${process.env.PORT}!`))
+
+// app.get('/', (req, res) => res.send("Hello World!"))

c116a0a35c8d6da963c2ff171244fcc2d417cae9
maulanairfanf <maulanairfanf@gmail.com>
2024-07-04 11:41:03 +0700

    try deploy to netlify

--- app.js
+++ app.js
@@ -1,47 +1,42 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const cors = require('cors')
+const serverless = require('serverless-http')
 
-const app = express();
+const app = express()
+const v1 = '/api/v1'
 
-// router
+const authCMSRouter = require('./app/api/v1/auth/router')
+const customerRouter = require('./app/api/v1/customer/router')
+const productRouter = require('./app/api/v1/product/router')
+const cartRouter = require('./app/api/v1/cart/router')
+const dashboardRouter = require('./app/api/v1/dashboard/router')
 
-const authCMSRouter = require('./app/api/v1/auth/router');
-const customerRouter = require('./app/api/v1/customer/router');
-const productRouter = require('./app/api/v1/product/router');
-const cartRouter = require('./app/api/v1/cart/router');
-const dashboardRouter = require('./app/api/v1/dashboard/router');
+const notFoundMiddleware = require('./app/middlewares/not-found')
+const handleErrorMiddleware = require('./app/middlewares/handler-error')
 
-
-const v1 = '/api/v1';
-
-const notFoundMiddleware = require('./app/middlewares/not-found');
-const handleErrorMiddleware = require('./app/middlewares/handler-error');
-app.use(cors());
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Welcome to api volkpedia',
-  });
-});
+	res.status(200).json({
+		message: 'Welcome to api volkpedia',
+	})
+})
 
+app.use(`${v1}/cms`, authCMSRouter)
+app.use(`${v1}`, customerRouter)
+app.use(`${v1}`, productRouter)
+app.use(`${v1}`, cartRouter)
+app.use(`${v1}/cms`, dashboardRouter)
 
-app.use(`${v1}/cms`, authCMSRouter);
-app.use(`${v1}`, customerRouter);
-app.use(`${v1}`, productRouter);
-app.use(`${v1}`, cartRouter);
-app.use(`${v1}/cms`, dashboardRouter);
+app.use(notFoundMiddleware)
+app.use(handleErrorMiddleware)
 
-
-
-app.use(notFoundMiddleware);
-app.use(handleErrorMiddleware);
-
-module.exports = app;
+module.exports.handler = serverless(app)

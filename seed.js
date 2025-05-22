03235ac43ee4abe43716745289de3dff0ca6b129
maulanairfanf <maulanairfanf@gmail.com>
2025-05-22 16:12:45 +0700

    update seed

--- seed.js
+++ seed.js
@@ -4,6 +4,9 @@ const fs = require('fs')
 const path = require('path')
 const mongoose = require('mongoose')
 const { urlDb } = require('./app/config')
+const Customer = require('./app/api/v1/customer/model.js')
+const Product = require('./app/api/v1/product/model.js')
+const User = require('./app/api/v1/users/model.js') // perbaiki path jika foldernya 'users'
 
 // Connect ke MongoDB
 mongoose.connect(urlDb, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -17,30 +20,35 @@ db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', () => {
 	console.log('Connected to MongoDB')
 
-	async function seedCollection(collectionName, filePath) {
+	async function seedCollection(model, filePath) {
 		try {
 			const data = fs.readFileSync(filePath, 'utf8')
 			const jsonData = JSON.parse(data)
-
-			const collection = db.collection(collectionName)
-			const result = await collection.insertMany(jsonData)
-			console.log(`${collectionName} imported successfully`, result)
+			const result = await model.insertMany(jsonData, { ordered: false })
+			console.log(`${model.modelName} imported successfully`, result)
 		} catch (err) {
-			console.error(`Error importing ${collectionName}:`, err.message)
+			if (
+				err.code === 11000 ||
+				(err.writeErrors && err.writeErrors.every(e => e.code === 11000))
+			) {
+				console.warn(`Some duplicates skipped in ${model.modelName}.`)
+			} else {
+				console.error(`Error importing ${model.modelName}:`, err.message)
+			}
 		}
 	}
 
 	async function seedDatabase() {
 		await seedCollection(
-			'customers',
+			Customer,
 			path.join(__dirname, 'app', 'mock', 'customers.json')
 		)
 		await seedCollection(
-			'products',
+			Product,
 			path.join(__dirname, 'app', 'mock', 'products.json')
 		)
 		await seedCollection(
-			'users',
+			User,
 			path.join(__dirname, 'app', 'mock', 'users.json')
 		)
 
